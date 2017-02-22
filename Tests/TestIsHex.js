9e92e6c39ee43ad016c448f36b02b39c7a621126
Brandon Mowat <brandonmowat11@gmail.com>
2017-02-22 15:09:04 -0500

    🛠  fixed crucial bugs

--- Tests/TestIsHex.js
+++ Tests/TestIsHex.js
@@ -1,1 +1,17 @@
-import {isHexColor, isHex} from '../index.js';
+var IsHex = require('../index.js');
+
+function assertEquals(result, expected) {
+  return result == expected;
+}
+
+function testSuite() {
+  // Write tests here...
+  return assertEquals(IsHex.isHex("000"), true) &&
+    assertEquals(IsHex.isHex("ggdeqwd2d2g"), false) &&
+    assertEquals(IsHex.isHex("fFfFfF"), true) &&
+    assertEquals(IsHex.isHexColor("1FfeFf"), true) &&
+    assertEquals(IsHex.isHexColor("effefg"), false) &&
+    assertEquals(IsHex.isHexColor("geffefg"), false)?"All tests passed👌🏻":"failed..."
+}
+
+console.log(testSuite());
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 function isHexColor(input) {
-  var hexPattern = /([a-f]|\d){6}/;
-  return hexPattern.test(input);
+  var hexPattern = /([A-F]|[a-f]|\d){6}/;
+  return input.match(hexPattern) === null?false:input.match(hexPattern)[0] === input;
 }
 
 function isHex(input) {
-  var hexPattern = /([a-f]|\d)*/;
-  return hexPattern.test(input)
+  var hexPattern = /([A-F]|[a-f]|\d)*/;
+  return input.match(hexPattern)[0] === input;
 }
 
-function testIsHex() {
-  console.log(isHex('ffffff'));
-  console.log(isHex('0000000000'));
-  console.log(isHexColor('a0a0a0'));
-  console.log(isHexColor('asdfgh'));
-}
-
-testIsHex();
+module.exports = {isHex, isHexColor};
